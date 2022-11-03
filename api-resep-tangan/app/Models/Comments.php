3090fe812348ad3308ba2c18208253f17fba2197
synraax <caesariodito2014@gmail.com>
2022-11-03 16:34:44 +0700

    init migration

---
+++ api-resep-tangan/app/Models/Comments.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comments extends Model
+{
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'message',
+        'recipe_id',
+        'user_id',
+    ];
+}
---
+++ api-resep-tangan/database/migrations/2022_11_03_085859_create_contents_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('recipe_id')->constrained('recipes');
+            $table->string('image');
+            $table->string('description');
+            $table->string('video');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('content');
+    }
+};
